58dc59b0d6b1402cb2156554790a852fcfbf1f4c
Apeksha Mishra <119792544+apeksham22@users.noreply.github.com>
2026-08-10 07:48:12 +0000

    compare

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); //Comparisons convert null to a number, treating it as 0
+
+// === => Strict equality operator 
+console.log(null === 0); //Strict equality does not perform type conversion
+console.log("2" === 2);
