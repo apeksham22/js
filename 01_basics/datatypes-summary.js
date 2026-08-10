ca742b66be5203f6cd21381105a9edbb4b1139a6
Apeksha Mishra <119792544+apeksham22@users.noreply.github.com>
2026-08-10 07:50:38 +0000

    change in location of file

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,34 @@
+//  Primitive
+
+// 7 types: String, Number, Boolean, undefined, null, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ['shaktiman', 'naagraj', 'doga'];
+let myObj = {
+    name: "apeksha",
+    age: 22
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
