0063dcd84effa2b1be0459d45a7bf398ba9a4c7d
Apeksha Mishra <119792544+apeksham22@users.noreply.github.com>
2026-08-10 08:23:00 +0000

    comparison

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -22,5 +22,40 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 33 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *************************** Operations *************************
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Apeksha!"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2); //if first operand is string, then all operands will be traeted as string
+// console.log(1+2+"2"); //if last operand is string, then all conersions will be done before that string
+
+// console.log(true);
+// console.log(+true);
+// console.log(true+); //error
+// console.log(+"");
+
+let num1, num2, num3;
+ num1 = num2 = num3 = 2+2
+
+ let gameCounter = 100
+ ++gameCounter
+ console.log(gameCounter)
