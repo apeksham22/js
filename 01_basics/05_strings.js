668a86ac59d59f843ef98d1234cec0ecf92639b0
Apeksha Mishra <119792544+apeksham22@users.noreply.github.com>
2026-08-17 10:09:27 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,11 +1,11 @@
-const name = "hitesh"
+const name = "apeksha"
 const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hitesh-hc-com')
+const gameName = new String('apeksha-am-com')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -22,11 +22,11 @@ console.log(newString);
 const anotherString = gameName.slice(-8, 4)
 console.log(anotherString);
 
-const newStringOne = "   hitesh    "
+const newStringOne = "   apeksha    "
 console.log(newStringOne);
 console.log(newStringOne.trim());
 
-const url = "https://hitesh.com/hitesh%20choudhary"
+const url = "https://apeksha.com/apeksha%20choudhary"
 
 console.log(url.replace('%20', '-'))
 
