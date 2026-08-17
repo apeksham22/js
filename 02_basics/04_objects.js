1eaabc72389788cf7efd5b62c181ccd53e4787e1
Apeksha Mishra <119792544+apeksham22@users.noreply.github.com>
2026-08-17 10:15:49 +0000

    json talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -2,7 +2,7 @@
 const tinderUser = {}
 
 tinderUser.id = "123abc"
-tinderUser.name = "Sammy"
+tinderUser.name = "Khushu"
 tinderUser.isLoggedIn = false
 
 // console.log(tinderUser);
@@ -11,8 +11,8 @@ const regularUser = {
     email: "some@gmail.com",
     fullname: {
         userfullname: {
-            firstname: "hitesh",
-            lastname: "choudhary"
+            firstname: "Apeksha",
+            lastname: "Mishra"
         }
     }
 }
@@ -33,15 +33,15 @@ const obj3 = {...obj1, ...obj2}
 const users = [
     {
         id: 1,
-        email: "h@gmail.com"
+        email: "a@gmail.com"
     },
     {
         id: 1,
-        email: "h@gmail.com"
+        email: "a@gmail.com"
     },
     {
         id: 1,
-        email: "h@gmail.com"
+        email: "a@gmail.com"
     },
 ]
 
@@ -58,7 +58,7 @@ users[1].email
 const course = {
     coursename: "js in hindi",
     price: "999",
-    courseInstructor: "hitesh"
+    courseInstructor: "apeksha"
 }
 
 // course.courseInstructor
@@ -69,7 +69,7 @@ const {courseInstructor: instructor} = course
 console.log(instructor);
 
 // {
-//     "name": "hitesh",
+//     "name": "apeksha",
 //     "coursename": "js in hindi",
 //     "price": "free"
 // }
